715f4290a1f1b432c530cdc59383fb33d91d98d0
abaker93 <anna.baker.design@gmail.com>
2020-09-17 22:35:40 -0400

    this doesn't work

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-const add = (x, y) => {
-	return x + y;
-}
+let displayValue = '0';
+let firstNum = null;
+let secondNum = null;
+let firstOperator = null;
+let secondOperator = null;
+const buttons = document.querySelectorAll('button');
+console.log(buttons);
 
-const subtract = (x, y) => {
-	return x - y;
-}
-
-const multiply = (x, y) => {
-	return x * y;
-}
-
-const divide = (x, y) => {
-	return x / y;
-}
+const add = (x, y) => { return x + y; }
+const subtract = (x, y) => { return x - y; }
+const multiply = (x, y) => { return x * y; }
+const divide = (x, y) => { return x / y; }
 
 const operate = (x, y, operator) => {
 	if (operator === 'add'){
@@ -25,3 +22,42 @@ const operate = (x, y, operator) => {
         return divide(x, y);
     }
 }
+
+const updateDisplay = () => {
+	const display = document.getElementById('display');
+	display.innerText = displayValue;
+}
+
+updateDisplay();
+
+const clickButton = () => {
+	for (let i=0; i<buttons.length; i++) {
+		buttons[i].addEventListener('click', function() {
+			if (buttons[i].classList.contains('btn-num')) {
+				inputNum(buttons[i].value);
+				updateDisplay();
+				console.log(displayValue);
+			}
+		})
+	}
+}
+
+const inputNum = num => {
+	if (firstNum === null) {
+		if (displayValue === '0' || displayValue === 0) {
+			displayValue = num;
+		} else if (displayValue === firstNum) {
+			displayValue = num;
+		} else {
+			displayValue += num;
+		}
+	} else {
+		if (displayValue === firstNum) {
+			displayValue = num;
+		} else {
+			displayValue += num;
+		}
+	}
+}
+
+clickButton();
